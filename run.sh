9ced647cfd40837b8b9a287d3ff2f3d48563f809
waka <liangnida@163.com>
2019-07-15 16:46:33 +0800

    sh

--- run.sh
+++ run.sh
@@ -4,9 +4,13 @@ cd insurance_bot
 export PYTHONPATH="."
 export ROOT_DIR="root"
 
-/root/anaconda3/envs/py36/bin/python classification/run_classifier.py
+#/root/anaconda3/envs/py36/bin/python classification/run_classifier.py
 
 #/root/anaconda3/envs/py36/bin/python classification_2/run_classifier_2.py
 
 
 #/root/anaconda3/envs/py36/bin/python classification/run_classifier_focal_loss.py
+
+
+/root/anaconda3/envs/py36/bin/python log_process/log_eval.py
+
